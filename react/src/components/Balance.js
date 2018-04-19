748a32bc4c0021f65ae60a2ea51eef217500d092
Garrett Davis <garrett@imphatic.com>
2018-04-18 23:45:54 -0700

    fixed interest negative UI error

--- react/src/components/Balance.js
+++ react/src/components/Balance.js
@@ -4,14 +4,15 @@ import { Well, Grid, Row, Col } from 'react-bootstrap';
 export default class Balance extends React.Component {
 
     render() {
-        const payoff = Math.abs((this.props.interest*-1) + this.props.balance)
+        const interest = this.props.interest * -1
+        const payoff = Math.abs(interest + this.props.balance)
         return (
 
             <div>
                <Well bsSize="large">
                    <Grid>
                         <Row>
-                            <Col sm={3}><strong>Interest Accrued:</strong> ${this.props.interest.toFixed(2)} </Col>
+                            <Col sm={3}><strong>Interest Accrued:</strong> ${interest.toFixed(2)} </Col>
                             <Col sm={3}><strong>Principal Balance:</strong> ${this.props.balance.toFixed(2)}</Col>
                             <Col sm={3}><strong>Pay Off:</strong> ${payoff.toFixed(2)}</Col>
                         </Row>
